c02d168cbb975a468183bdd33a8a3ccd68d07048
Attila Repka <repka.attila@yandex.com>
2023-07-15 17:26:47 +0200

    feat: update existing headers

--- tests/header/header_spec.lua
+++ tests/header/header_spec.lua
@@ -1,5 +1,63 @@
 require("plenary.reload").reload_module("header", true)
 
+local function get_buffer_without_date(buffer, comments, constants)
+    result = {}
+    for _, line in ipairs(buffer) do
+        if not line:match("^%" .. comments.comment .. " " .. constants.date_created) then
+            table.insert(result, line)
+        end
+    end
+    return result
+end
+
+local function build_minimal_expected_comments(file_name, comments, header)
+    local result = {
+        comments.comment .. " " .. header.constants.file_name .. " " .. file_name,
+        comments.comment .. " " .. header.config.line_separator,
+        "",
+        file_name,
+    }
+
+    if comments.comment_start ~= nil then
+        result = {
+            comments.comment_start,
+            comments.comment .. " " .. header.constants.file_name .. " " .. file_name,
+            comments.comment .. " " .. header.config.line_separator,
+            comments.comment_end,
+            "",
+            file_name,
+        }
+    end
+    return result
+end
+
+local function build_extended_expected_comments(file_name, comments, constants, config)
+    local result = {
+        comments.comment .. " " .. constants.file_name .. " " .. file_name,
+        comments.comment .. " " .. constants.project .. " " .. config.project,
+        comments.comment .. " " .. constants.author .. " " .. config.author,
+        comments.comment .. " " .. config.line_separator,
+        comments.comment .. " " .. config.copyright_text,
+        "",
+        file_name,
+    }
+
+    if comments.comment_start ~= nil then
+        result = {
+            comments.comment_start,
+            comments.comment .. " " .. constants.file_name .. " " .. file_name,
+            comments.comment .. " " .. constants.project .. " " .. config.project,
+            comments.comment .. " " .. constants.author .. " " .. config.author,
+            comments.comment .. " " .. config.line_separator,
+            comments.comment .. " " .. config.copyright_text,
+            comments.comment_end,
+            "",
+            file_name,
+        }
+    end
+    return result
+end
+
 describe("setup", function()
     local header = require("header")
     it("setup with default configs", function()
@@ -32,10 +90,10 @@ describe("setup", function()
 end)
 
 describe("add_headers", function()
-    require("plenary.reload").reload_module("header", true)
-    local header = require("header")
-    local filetypes = require("filetypes")
     it("should insert headers to file depending on file type", function()
+        require("plenary.reload").reload_module("header", true)
+        local header = require("header")
+        local filetypes = require("filetypes")
         for k, v in pairs(filetypes) do
             vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
             local file_name = "main." .. k
@@ -47,35 +105,41 @@ describe("add_headers", function()
             local buffer = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
             local comments = v()
-            local expected = {
-                comments.comment .. " " .. header.constants.file_name .. " " .. file_name,
-                comments.comment .. " ------",
-                "",
-                file_name,
-            }
 
-            if comments.comment_start ~= nil then
-                expected = {
-                    comments.comment_start,
-                    comments.comment .. " " .. header.constants.file_name .. " " .. file_name,
-                    comments.comment .. " ------",
-                    comments.comment_end,
-                    "",
-                    file_name,
-                }
-            end
+            local expected = build_minimal_expected_comments(file_name, comments, header)
 
-            local buffer_without_date = {}
-            for _, line in ipairs(buffer) do
-                if not line:match("^%" .. comments.comment .. " " .. header.constants.date_created) then
-                    table.insert(buffer_without_date, line)
-                end
-            end
+            local buffer_without_date = get_buffer_without_date(buffer, comments, header.constants)
+
+            assert.are.same(expected, buffer_without_date)
+        end
+    end)
+    it("should insert headers via autocommand", function()
+        require("plenary.reload").reload_module("header", true)
+        local header = require("header")
+        local filetypes = require("filetypes")
+        for k, v in pairs(filetypes) do
+            vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+            local file_name = "main." .. k
+            vim.fn.setline(1, file_name)
+            vim.api.nvim_buf_set_name(0, file_name)
+
+            vim.api.nvim_command("AddHeader")
+
+            local buffer = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+            local comments = v()
+
+            local expected = build_minimal_expected_comments(file_name, comments, header)
+
+            local buffer_without_date = get_buffer_without_date(buffer, comments, header.constants)
 
             assert.are.same(expected, buffer_without_date)
         end
     end)
     it("should insert additional brief information to header", function()
+        require("plenary.reload").reload_module("header", true)
+        local header = require("header")
+        local filetypes = require("filetypes")
         for k, v in pairs(filetypes) do
             vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
             local file_name = "main." .. k
@@ -98,36 +162,45 @@ describe("add_headers", function()
             local buffer = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
             local comments = v()
-            local expected = {
-                comments.comment .. " File name: " .. file_name,
-                comments.comment .. " Project: " .. config.project,
-                comments.comment .. " Author: " .. config.author,
-                comments.comment .. " ------",
-                comments.comment .. " " .. config.copyright_text,
-                "",
-                file_name,
+            local expected = build_extended_expected_comments(file_name, comments, header.constants, config)
+
+            local buffer_without_date = get_buffer_without_date(buffer, comments, header.constants)
+
+            assert.are.same(expected, buffer_without_date)
+        end
+    end)
+    it("should update existing header files", function()
+        require("plenary.reload").reload_module("header", true)
+        local header = require("header")
+        local filetypes = require("filetypes")
+        for k, v in pairs(filetypes) do
+            vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+            local file_name = "main." .. k
+            vim.fn.setline(1, file_name)
+            vim.api.nvim_buf_set_name(0, file_name)
+
+            header.add_headers()
+            local buffer = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+            local comments = v()
+
+            local config = {
+                file_name = true,
+                author = "test_author",
+                project = "test_project",
+                date_created = true,
+                date_created_fmt = "%Y-%m-%d %H:%M:%S",
+                line_separator = "------",
+                copyright_text = "test_copyright",
             }
+            -- update config with extended stuff
+            header.setup(config)
+            header.add_headers()
 
-            if comments.comment_start ~= nil then
-                expected = {
-                    comments.comment_start,
-                    comments.comment .. " File name: " .. file_name,
-                    comments.comment .. " Project: " .. config.project,
-                    comments.comment .. " Author: " .. config.author,
-                    comments.comment .. " ------",
-                    comments.comment .. " " .. config.copyright_text,
-                    comments.comment_end,
-                    "",
-                    file_name,
-                }
-            end
+            buffer = vim.api.nvim_buf_get_lines(0, 0, -1, false)
 
-            local buffer_without_date = {}
-            for _, line in ipairs(buffer) do
-                if not line:match("^%" .. comments.comment .. " " .. header.constants.date_created) then
-                    table.insert(buffer_without_date, line)
-                end
-            end
+            local expected = build_extended_expected_comments(file_name, comments, header.constants, config)
+
+            local buffer_without_date = get_buffer_without_date(buffer, comments, header.constants)
 
             assert.are.same(expected, buffer_without_date)
         end
