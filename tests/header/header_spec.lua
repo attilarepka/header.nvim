5626667d3d567e37d1d2b0fc58237d70dcb4d988
Samuel Boyden <sam@stboyden.com>
2025-08-11 17:24:47 +0100

    [Feature] Add ability to configure comment style (#19)
    * feat: add ability to configure comment style
    
    Adds the config option `use_block_header` to optionally prefer the
    block-style comment in a given language (if `true`) over the single-line
    comment (if `false`).
    
    By default, the value is `true` to maintain the original behaviour,
    which preferred block-style comments over multiple single-line comments.
    
    * test: add for block checking block headers & modify existing tests
    
    * fix: add missing use_block_header check to c++
    
    * docs: add `use_block_header` usage in config and setup
    
    * fix: pass use_block_header to comment fn in update_date_modified function
    
    * fix: actually check if use_block_header is nil (stupid)
    
    * fix: remove accidentally included header
    
    * refactor(#19): languages to return a table with two tables (block & line) `{ start: string, line: string, ["end"]: string }`
    
    Tests changed to compensate - all tests pass (please check thoroughly).
    
    Changes related to comment <https://github.com/attilarepka/header.nvim/pull/19#issuecomment-3172734803>.
    
    * fix: lua end block comment
    
    * fix: last line header detection when using line comments over block comments
    
    Revert "fix: last line header detection when using line comments over block comments"
    
    This reverts commit 8cdb422552d3ffefb604a020515e5c1419239d4d.
    
    * fix: accomodate maybe previously present block comment header
    
    * refactor: merge end of block detection and normal line comment end detection
    
    Block detection is now maybe less efficient, but I don't see it
    impacting performance to a noticable degree - as there doesn't seem to
    be much difference in the actual work being done.
    
    * chore: remove commented-out `find_block_comment_end` and remove stylua ignores

--- tests/header/header_spec.lua
+++ tests/header/header_spec.lua
@@ -10,11 +10,20 @@ local function get_modified_date(buffer)
 end
 
 local function get_buffer_without_date(buffer, comments, constants)
+    local style
+    if header.config.use_block_header and comments.block and comments.block.start then
+        style = comments.block
+    elseif comments.line and comments.line.line then
+        style = comments.line
+    else
+        style = comments.block or comments.line
+    end
+
     result = {}
     for _, line in ipairs(buffer) do
         if
-            not line:match("^%" .. comments.comment .. " " .. constants.date_created)
-            and not line:match("^%" .. comments.comment .. " " .. constants.date_modified)
+            not line:match("^%" .. style.line .. " " .. constants.date_created)
+            and not line:match("^%" .. style.line .. " " .. constants.date_modified)
         then
             table.insert(result, line)
         end
@@ -23,19 +32,28 @@ local function get_buffer_without_date(buffer, comments, constants)
 end
 
 local function build_minimal_expected_comments(file_name, comments, header)
+    local style
+    if header.config.use_block_header and comments.block and comments.block.start then
+        style = comments.block
+    elseif comments.line and comments.line.line then
+        style = comments.line
+    else
+        style = comments.block or comments.line
+    end
+
     local result = {
-        comments.comment .. " " .. header.constants.file_name .. " " .. file_name,
-        comments.comment .. " " .. header.config.line_separator,
+        style.line .. " " .. header.constants.file_name .. " " .. file_name,
+        style.line .. " " .. header.config.line_separator,
         "",
         file_name,
     }
 
-    if comments.comment_start ~= nil then
+    if style.start and style["end"] then
         result = {
-            comments.comment_start,
-            comments.comment .. " " .. header.constants.file_name .. " " .. file_name,
-            comments.comment .. " " .. header.config.line_separator,
-            comments.comment_end,
+            style.start,
+            style.line .. " " .. header.constants.file_name .. " " .. file_name,
+            style.line .. " " .. header.config.line_separator,
+            style["end"],
             "",
             file_name,
         }
@@ -44,25 +62,34 @@ local function build_minimal_expected_comments(file_name, comments, header)
 end
 
 local function build_extended_expected_comments(file_name, comments, constants, config)
+    local style
+    if config.use_block_header and comments.block and comments.block.start then
+        style = comments.block
+    elseif comments.line and comments.line.line then
+        style = comments.line
+    else
+        style = comments.block or comments.line
+    end
+
     local result = {
-        comments.comment .. " " .. constants.file_name .. " " .. file_name,
-        comments.comment .. " " .. constants.project .. " " .. config.project,
-        comments.comment .. " " .. constants.author .. " " .. config.author,
-        comments.comment .. " " .. config.line_separator,
-        comments.comment .. " " .. config.copyright_text,
+        style.line .. " " .. constants.file_name .. " " .. file_name,
+        style.line .. " " .. constants.project .. " " .. config.project,
+        style.line .. " " .. constants.author .. " " .. config.author,
+        style.line .. " " .. config.line_separator,
+        style.line .. " " .. config.copyright_text,
         "",
         file_name,
     }
 
-    if comments.comment_start ~= nil then
+    if style.start and style["end"] then
         result = {
-            comments.comment_start,
-            comments.comment .. " " .. constants.file_name .. " " .. file_name,
-            comments.comment .. " " .. constants.project .. " " .. config.project,
-            comments.comment .. " " .. constants.author .. " " .. config.author,
-            comments.comment .. " " .. config.line_separator,
-            comments.comment .. " " .. config.copyright_text,
-            comments.comment_end,
+            style.start,
+            style.line .. " " .. constants.file_name .. " " .. file_name,
+            style.line .. " " .. constants.project .. " " .. config.project,
+            style.line .. " " .. constants.author .. " " .. config.author,
+            style.line .. " " .. config.line_separator,
+            style.line .. " " .. config.copyright_text,
+            style["end"],
             "",
             file_name,
         }
@@ -84,6 +111,7 @@ describe("setup", function()
             date_modified = true,
             date_modified_fmt = "%Y-%m-%d %H:%M:%S",
             line_separator = "------",
+            use_block_header = true,
             copyright_text = nil,
             license_from_file = false,
         }
@@ -101,6 +129,7 @@ describe("setup", function()
             date_modified = true,
             date_modified_fmt = "%Y-%m-%d %H:%M:%S",
             line_separator = "------",
+            use_block_header = false,
             copyright_text = "test_copyright",
             license_from_file = false,
         }
@@ -172,6 +201,7 @@ describe("add_headers", function()
                 date_modified = true,
                 date_modified_fmt = "%Y-%m-%d %H:%M:%S",
                 line_separator = "------",
+                use_block_header = true,
                 copyright_text = "test_copyright_text",
             }
             header.setup(config)
@@ -209,6 +239,7 @@ describe("add_headers", function()
                 date_modified = true,
                 date_modified_fmt = "%Y-%m-%d %H:%M:%S",
                 line_separator = "------",
+                use_block_header = true,
                 copyright_text = "test_copyright",
             }
             -- update config with extended stuff
@@ -222,6 +253,64 @@ describe("add_headers", function()
             local buffer_without_date = get_buffer_without_date(buffer, comments, header.constants)
 
             assert.are.same(expected, buffer_without_date)
+        end
+    end)
+    it("should prefer to use the single-line comment type over block comment", function()
+        local filetypes = require("filetypes")
+        for k, v in pairs(filetypes) do
+            vim.api.nvim_buf_set_lines(0, 0, -1, false, {})
+            local file_name = "main." .. k
+            vim.fn.setline(1, file_name)
+            vim.api.nvim_buf_set_name(0, file_name)
+
+            local config = {
+                file_name = true,
+                author = "test_author",
+                project = "test_project",
+                date_created = true,
+                date_created_fmt = "%Y-%m-%d %H:%M:%S",
+                date_modified = true,
+                date_modified_fmt = "%Y-%m-%d %H:%M:%S",
+                line_separator = "------",
+                use_block_header = false,
+                copyright_text = "test_copyright",
+            }
+
+            local comparison_config = {
+                file_name = true,
+                author = "test_author",
+                project = "test_project",
+                date_created = true,
+                date_created_fmt = "%Y-%m-%d %H:%M:%S",
+                date_modified = true,
+                date_modified_fmt = "%Y-%m-%d %H:%M:%S",
+                line_separator = "------",
+                use_block_header = true,
+                copyright_text = "test_copyright",
+            }
+
+            header.setup(config)
+
+            header.add_headers()
+
+            local buffer = vim.api.nvim_buf_get_lines(0, 0, -1, false)
+
+            local single_commented = v()
+            local block_commented = v()
+
+            local expected = build_extended_expected_comments(file_name, single_commented, header.constants, config)
+            local buffer_without_date = get_buffer_without_date(buffer, single_commented, header.constants)
+
+            assert.are.same(expected, buffer_without_date)
+
+            if single_commented.comment_start == nil and block_commented.comment_start ~= nil then
+                -- if comments and opposite_comments are different, we should not have the opposite
+                -- comments in the buffer, this should only be true for
+                -- languages that support both block and single-line comments
+                expected =
+                    build_extended_expected_comments(file_name, block_commented, header.constants, comparison_config)
+                assert.are.not_same(expected, buffer_without_date)
+            end
         end
     end)
 end)
@@ -255,6 +344,7 @@ describe("update_date_modified", function()
                 date_modified = true,
                 date_modified_fmt = "%Y-%m-%d %H:%M:%S",
                 line_separator = "------",
+                use_block_header = true,
                 copyright_text = "test_copyright",
             }
             -- update config with extended stuff
